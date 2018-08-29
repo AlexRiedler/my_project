f64720ea32ee13f7371019d2a72e2437291941ec
Alex Riedler <alex@riedler.ca>
2018-08-29 09:08:56 -0400

    Fix issue with webpack watch running in background after exiting.

--- config/dev.exs
+++ config/dev.exs
@@ -11,7 +11,7 @@ config :my_project, MyProjectWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [npm: ["run", "watch", cd: Path.expand("../assets", __DIR__)]]
+  watchers: [npm: ["run", "phoenix_watch", cd: Path.expand("../assets", __DIR__)]]
 
 # ## SSL Support
 #
